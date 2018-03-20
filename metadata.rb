3186566bc5e230bb7186cc48eeadc7be6a458cc6
Grant Shively <gshively@godaddy.com>
2018-03-20 11:09:29 -0700

    Bumped version to avoid conflict with upstream

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '6.0.2'
+version          '600.0.2'
 
 recipe 'jenkins::master', 'Installs a Jenkins master'
 
